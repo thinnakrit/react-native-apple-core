8208290bdd1a3f11b9003fb3a2ea211e579b4110
Jay <thinnakrit.idesign@gmail.com>
2019-12-12 14:31:52 +0700

    remove container

--- libs/modules/SignInWithApple.js
+++ libs/modules/SignInWithApple.js
@@ -3,20 +3,29 @@ import { Text, View, Dimensions } from 'react-native'
 import { WebView } from 'react-native-webview'
 import PropTypes from 'prop-types'
 // ---
-import SignInWithAppleContainer from './SignInWithApple/SignInWithAppleContainer'
 import RBSheet from "react-native-raw-bottom-sheet"
+import SignInWithAppleService from '../utils/SignInWithAppleService'
 
 class SignInWithApple extends Component {
 
   constructor(props) {
     super(props)
+
+    this.state = {
+      isStartCallService: false,
+      response_type: "id_token code",
+      response_mode: "fragment",
+    }
+
+    this.onSignIn = this.onSignIn.bind(this);
+    this.onNavigationStateChange = this.onNavigationStateChange.bind(this);
     this.openSignIn = this.openSignIn.bind(this);
     this.handleNavigationStateChange = this.handleNavigationStateChange.bind(this);
     this.handleCallBack = this.handleCallBack.bind(this);
   }
 
   openSignIn = () => {
-    this.signInContainerRef.onSignIn()
+    this.onSignIn()
     this.actionSheetRef.open()
   }
 
@@ -27,64 +36,105 @@ class SignInWithApple extends Component {
   handleCallBack = (result) => {
     const { onSuccess } = this.props
     onSuccess(result)
+    this.setState({
+      isStartCallService: false
+    })
     this.actionSheetRef.close()
   }
 
+  onSignIn = () => {
+    this.setState({
+      isStartCallService: true
+    })
+  }
+
+  onNavigationStateChange = (webViewState) => {
+    const { onCallBack } = this.props
+    const appleUri = "https://appleid.apple.com"
+    const getParam = webViewState.url.split("/auth/")
+    if (getParam[0] !== appleUri) {
+      const getQuery = webViewState.url.split("#")
+      if (getQuery.length > 0) {
+        const getData = getQuery[1].split("&")
+        const getValue = getData.reduce((prev, curr) => {
+          const value = curr.split("=")
+          if (value[1]) {
+            prev[value[0]] = value[1]
+          }
+          return prev
+        }, {})
+        if (getValue) {
+          this.setState({
+            isStartCallService: false
+          })
+          onCallBack(getValue)
+        }
+      }
+    }
+  }
+
   render() {
-    const { initialConfig } = this.props
+    const { initialConfig: {
+      redirect_uri,
+      client_id,
+      state,
+    } } = this.props
     const screenWidth = Math.round(Dimensions.get('window').width)
     const screenHeight = Math.round(Dimensions.get('window').height)
+
+    const {
+      isStartCallService,
+      response_type,
+      response_mode,
+    } = this.state
+
+    const getUri = SignInWithAppleService.create({
+      response_type: response_type,
+      response_mode: response_mode,
+      client_id: client_id,
+      redirect_uri: redirect_uri,
+      state: state
+    })
+
     return (
-      <SignInWithAppleContainer
-        initialConfig={ initialConfig }
-        ref={ref => this.signInContainerRef = ref }
-        onCallBack={this.handleCallBack}
-        render={({
-          getUri,
-          isStartCallService,
-        }) => {
-          return (
-            <Fragment>
-              <RBSheet
-                ref={ref => {
-                  this.actionSheetRef = ref;
+      <Fragment>
+        <RBSheet
+          ref={ref => {
+            this.actionSheetRef = ref;
+          }}
+          closeOnDragDown={true}
+          closeOnPressMask={false}
+          height={screenHeight - 100}
+          duration={250}
+          customStyles={{
+            container: {
+              justifyContent: "center",
+              alignItems: "center"
+            }
+          }}
+        >
+          {isStartCallService ? (
+            <View style={{flex: 1, height: screenHeight - 100}}>
+              <WebView
+                ref="webview"
+                source={{ uri: getUri }}
+                onNavigationStateChange={this.handleNavigationStateChange}
+                javaScriptEnabled = {true}
+                domStorageEnabled = {true}
+                startInLoadingState={false}
+                style={{
+                  flex: 1,
+                  resizeMode: 'cover',
+                  width: screenWidth,
+                  height: screenHeight - 100,
                 }}
-                closeOnDragDown={true}
-                closeOnPressMask={false}
-                height={screenHeight - 100}
-                duration={250}
-                customStyles={{
-                  container: {
-                    justifyContent: "center",
-                    alignItems: "center"
-                  }
-                }}
-              >
-                {isStartCallService ? (
-                  <View style={{flex: 1, height: screenHeight - 100}}>
-                    <WebView
-                      ref="webview"
-                      source={{ uri: getUri }}
-                      onNavigationStateChange={this.handleNavigationStateChange}
-                      javaScriptEnabled = {true}
-                      domStorageEnabled = {true}
-                      startInLoadingState={false}
-                      style={{
-                        flex: 1,
-                        resizeMode: 'cover',
-                        width: screenWidth,
-                        height: screenHeight - 100,
-                      }}
-                    />
-                  </View>
-                ) : (
-                  <Fragment />
-                )}
-              </RBSheet>
-            </Fragment>
-          )
-        }}
-      />
+              />
+            </View>
+          ) : (
+            <Fragment />
+          )}
+        </RBSheet>
+      </Fragment>
     )
   }
 }
