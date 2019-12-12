d9fedafc32ab17c92247eee7a4c6bbd3923e703f
Jay <thinnakrit.idesign@gmail.com>
2019-12-12 13:48:55 +0700

    update source

--- android/app/src/main/java/com/rnapplecore/MainApplication.java
+++ android/app/src/main/java/com/rnapplecore/MainApplication.java
@@ -4,8 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
